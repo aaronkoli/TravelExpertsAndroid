aa7956c4a7f064629003c741cd7ad8d5f47074f8
velveteen-rever <velveteen.rever@gmail.com>
2023-05-04 22:01:09 -0600

    added volley dependency and network permission tag

--- app/src/main/java/com/example/travelexpertsandroid/AllCustomersView.java
+++ app/src/main/java/com/example/travelexpertsandroid/AllCustomersView.java
@@ -3,12 +3,22 @@ package com.example.travelexpertsandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class AllCustomersView extends AppCompatActivity {
+    ListView lvCustomers;
+    ArrayAdapter<Customer> adapter;
+    Customer selection;
+    String apiUrl = "http://localhost:8080/Workshop7MicahREST_war_exploded/";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_customers_view);
+
+        lvCustomers = findViewById(R.id.lvCustomers);
     }
 }
